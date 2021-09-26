95a389b90e09f0abf8e461f8d8e0c7f2318d7f8e
Ben Kaan <benkaan001@gmail.com>
2021-09-25 23:24:01 -0500

    set up event listeners

--- script.js
+++ script.js
@@ -1,4 +1,57 @@
+//step 1 define var score, startScreen, gameArea
 
+// step 2 add a click event for the startScreen
+
+// step 3 add event listenerss pressOn and pressOff and define the functions
+
+//step 4 set up an object named keys that tracks which keys are being used by the user
+
+var score = document.querySelector(".score");
+
+var startScreen = document.querySelector(".startScreen");
+
+var gameArea = document.querySelector(".gameArea");
+
+//step 4
+
+var keys ={ArrowUp:false, ArrowDown:false, ArrowRigh:false, ArrowLeft:false};
+
+
+// step 2 add an event listener
+
+startScreen.addEventListener("click", start); 
+
+
+// step 3 add event listener 
+
+document.addEventListener("keydown", pressOn);
+document.addEventListener("keyup", pressOff);
+
+// function pressOn(event){
+//     event.preventDefault();
+//     console.log("on",event.key);
+// };
+
+function pressOn(event){
+    event.preventDefault();
+    keys[event.key]= true;
+    console.log(keys);
+};
+
+// function pressOff(event){
+//     event.preventDefault();
+//     console.log("off", event.key);
+// };
+
+function pressOff(event){
+    event.preventDefault();
+    keys[event.key]= false;
+    console.log(keys);
+};
+
+function start(){
+    console.log("click");
+}
 
 
 
